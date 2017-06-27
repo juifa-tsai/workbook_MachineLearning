2914cfdc727eb021a6944893fc8cc9dff1ccc98d
Jui-Fa Tsai <alpha78178@gmail.com>
2017-06-27 23:03:04 +0800

    Create and update README

--- Python_ML_SR/Chapter_02/Perceptron.py
+++ Python_ML_SR/Chapter_02/Perceptron.py
@@ -8,16 +8,19 @@ class Perceptron(object):
         self.n_iter = n_iter
 
     def fit(self, X, y):
-        self.w_ = np.zeros(1 + X.shape[1])
+        self.w_ = np.zeros(1 + X.shape[1]) # initial w_ = [w_0,w_1....w_features] = 0
         self.errors_ = []
 
         for _ in range(self.n_iter):
             errors = 0
 
             for xi, target in zip(X, y):
+                # dw = eta.dy
                 update = self.eta * (target - self.predict(xi))
+                # dw = eta.dy.X
                 self.w_[1:] += update * xi
-                self.w_[0] += update
+                self.w_[0] += update # dy.x_0
+                # Number of misclassifications in each iteration (epoch)
                 errors += int(update != .0)
 
             self.errors_.append(errors)
@@ -25,7 +28,9 @@ class Perceptron(object):
         return self
 
     def net_input(self, X):
+        # w.X = w'.X' + w_0.X_0, (X_0 = 1)
         return np.dot(X, self.w_[1:]) + self.w_[0]
 
     def predict(self, X):
+        # y = sign(w.X)
         return np.where(self.net_input(X) >= 0.0, 1, -1)
