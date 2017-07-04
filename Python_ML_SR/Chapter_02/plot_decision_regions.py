3b82031a1fba0cc507e2af77a5e6c7804539dcd0
Jui-Fa Tsai <alpha78178@gmail.com>
2017-07-04 22:15:09 +0800

    Add comments for helping understand

--- Python_ML_SR/Chapter_02/plot_decision_regions.py
+++ Python_ML_SR/Chapter_02/plot_decision_regions.py
@@ -3,19 +3,29 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 def plot_decision_regions(X, y, classifier, resolution=0.02):
-    # setup marker generator and color map
+    ### setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'ligtgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
-    # plot the decision suface
-    x1_min = X[:, 0].min() - 1
-    x1_max = X[:, 0].max() + 1
+    # np.unique(y) -> (1, -1)
+    # colors[:len(np.unique(y))] -> ('red', 'blue')
+
+    ### plot the decision suface
+    x1_min = X[:, 0].min() - 1 # extract the minimum of 0 colume-variable & expand with 1 cm
+    x1_max = X[:, 0].max() + 1 # extract the maximum of 0 colume-variable & expand with 1 cm
     x2_min = X[:, 1].min() - 1
     x2_max = X[:, 1].max() + 1
 
+    # Making a grid 2-D suface w.r.t. the resolution
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
+
+    # np.arange(x1_min, x1_max, resolution) -> 1-D array w.r.t. resolution bewteen min & max values
+    # len(np.arange(x1_min, x1_max, resolution)) -> 235 length
+    # len(np.arange(x2_min, x2_max, resolution)) -> 305 length
+    # len(xx1), 'x' ,len(xx1[0]) -> 305 x 235 grid
+    # len(xx2), 'x' ,len(xx2[0]) -> 305 x 235 grid
 
     Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
     Z = Z.reshape(xx1.shape)
