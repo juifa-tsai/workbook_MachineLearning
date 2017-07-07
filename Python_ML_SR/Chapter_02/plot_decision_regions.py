b34ccd5ed29a768e9b20a53ca5243c579e5b7e15
Jui-Fa Tsai <alpha78178@gmail.com>
2017-07-07 15:54:48 +0800

    Add decision PLA line, open the x lower range

--- Python_ML_SR/Chapter_02/plot_decision_regions.py
+++ Python_ML_SR/Chapter_02/plot_decision_regions.py
@@ -18,8 +18,10 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     x2_max = X[:, 1].max() + 1
 
     # Making a grid 2-D suface w.r.t. the resolution
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
+    #xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+    #                       np.arange(x2_min, x2_max, resolution))
+    xx1, xx2 = np.meshgrid(np.arange(0., x1_max, resolution),
+                           np.arange(0., x2_max, resolution))
 
     # np.arange(x1_min, x1_max, resolution) -> 1-D array w.r.t. resolution bewteen min & max values
     # len(np.arange(x1_min, x1_max, resolution)) -> 235 length
@@ -31,8 +33,10 @@ def plot_decision_regions(X, y, classifier, resolution=0.02):
     Z = Z.reshape(xx1.shape)
 
     plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
+    #plt.xlim(xx1.min(), xx1.max())
+    #plt.ylim(xx2.min(), xx2.max())
+    plt.xlim(0., xx1.max())
+    plt.ylim(0., xx2.max())
 
     # plot class samples
     for idx, c1 in enumerate(np.unique(y)):
