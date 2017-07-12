31d485cf28af7d492d6284cbd761fa34b9d1725e
Jui-Fa Tsai <alpha78178@gmail.com>
2017-07-12 22:50:53 +0800

    Add new subplots

--- Python_ML_SR/Chapter_02/Perceptron_online.py
+++ Python_ML_SR/Chapter_02/Perceptron_online.py
@@ -5,22 +5,27 @@ class Perceptron_online(object):
 
     def __init__(self, eta=0.01):
         self.eta = eta
-        self.error_ = 0
-        self.errorId_ = []
 
-    def fit(self, X, y):
+    def fit(self, X, y, max_iter=1):
         self.w_ = np.zeros(1 + X.shape[1]) # initial w_ = [w_0,w_1....w_features] = 0
 
         i = 0
-        while i < y.shape[0]:
-            update = self.eta * (y[i] - self.predict(X[i])) # = eta.dy
-            # dw = eta.dy.X = update.X
-            self.w_[1:] += update * X[i]
-            self.w_[0]  += update
-            if update != 0:
-                self.error_ += 1
-                self.errorId_.append(i)
-            i += 1
+        for it in range(max_iter):
+            n_error = 0
+            while i < y.shape[0]:
+                update = self.eta * (y[i] - self.predict(X[i])) # = eta.dy
+                # dw = eta.dy.X = update.X
+                self.w_[1:] += update * X[i]
+                self.w_[0]  += update
+                if update != 0:
+                    n_error += 1
+                i += 1
+
+            #if n_error is not 0:
+            #    print ' ', it, n_error
+            if n_error is 0:
+                #print ' stop: ', it
+                break
 
         return self
 
